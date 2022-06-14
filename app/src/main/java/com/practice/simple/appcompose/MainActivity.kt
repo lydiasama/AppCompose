fc6702d568a204b3f30df05f7606c8ce9564a3d9
Lydiasama <hallelujah.lydia@gmail.com>
2022-06-14 16:39:06 +0700

    Several preview modes test

--- app/src/main/java/com/practice/simple/appcompose/MainActivity.kt
+++ app/src/main/java/com/practice/simple/appcompose/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.practice.simple.appcompose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.practice.simple.appcompose.ui.theme.AppComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PreviewMessageCard()
+            AppComposeTheme {
+                PreviewMessageCard()
+            }
         }
     }
 }
@@ -33,26 +39,62 @@ fun MessageCard(name: String, color: Color = Color.Unspecified) {
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Icon(@DrawableRes imageRes: Int) {
+    AppComposeTheme {
+        Box(
+            Modifier
+                .size(48.dp)
+                .wrapContentSize(Alignment.Center)
+                .padding(10.dp)
+        ) {
+            Image(
+                painter = painterResource(imageRes),
+                contentDescription = "Icon"
+            )
+        }
+    }
+}
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    Row(modifier = Modifier.background(Color.White)) {
-        Column(
-            modifier = Modifier.padding(10.dp),
-            horizontalAlignment = Alignment.Start
-        ) {
-            MessageCard("Text1234567", Color.Blue)
-            MessageCard("Text3", Color.Cyan)
-        }
-
-        Column(
+    AppComposeTheme {
+        Row(
             modifier = Modifier
-                .padding(vertical = 10.dp)
-                .padding(end = 10.dp),
-            horizontalAlignment = Alignment.End
+                .fillMaxWidth()
         ) {
-            MessageCard("Text2345678", Color.Red)
-            MessageCard("Text4", Color.parse("#F556FF"))
+            Icon(R.drawable.ic_launcher_foreground)
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(vertical = 10.dp)
+                    .padding(end = 5.dp),
+                horizontalAlignment = Alignment.Start
+            ) {
+                MessageCard("Text1234567", Color.Blue)
+                MessageCard("Text3", Color.Cyan)
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp)
+                    .padding(start = 5.dp, end = 10.dp),
+                horizontalAlignment = Alignment.End
+            ) {
+                MessageCard("Text2345678", Color.Red)
+                MessageCard("Text4", Color.parse("#F556FF"))
+            }
         }
     }
 }
